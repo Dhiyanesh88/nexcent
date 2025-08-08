8f115d15a73d1446c047cd32f9bb8dd18b098a22
dhiyanesh <dhiyaneshrdhakrshnn@gmail.com>
2025-08-08 16:21:14 +0530

    h

--- src/webpages/header/header.jsx
+++ src/webpages/header/header.jsx
@@ -1,94 +1,86 @@
-
 import React from "react";
 import icon from "../../assets/img/icon.png";
 
-const Header = () => {
-  const headerStyle = {
-    display: "flex",
-    width: "100%",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    zIndex: 1000,
-    flexWrap: "wrap",
-    top: 0,
-    left: 0,
-    boxSizing: "border-box",
-    padding: "0 65px",
-    height: "80px",
-  };
-
-  const container = {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexWrap: "wrap",
-  };
-
-  const logoSection = {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-  };
-
-  const logoImage = {
-    width: "40px",
-    height: "25px",
-  };
-
-  const logoText = {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    color: "black",
-  };
-
-  const nav = {
-    display: "flex",
-    gap: "60px",
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-  };
-
-  const linkStyle = {
-    textDecoration: "none",
-    color: "#333",
-    fontWeight: 500,
-  };
-
-  const buttonGroup = {
-    display: "flex",
-    gap: "10px",
-  };
-
-  const loginBtn = {
-    padding: "0.6rem 1.2rem",
-    backgroundColor: "#fff",
-    color: "rgba(76, 175, 79, 1)",
-    borderRadius: "5px",
-    cursor: "pointer",
-    fontWeight: "bold",
-  };
-
-  const signupBtn = {
-    padding: "0.6rem 1.2rem",
-    backgroundColor: "rgba(76, 175, 79, 1)",
-    color: "#fff",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    fontWeight: "bold",
+function Header() {
+  const styles = {
+    headerStyle: {
+      display: "flex",
+      width: "100%",
+      alignItems: "center",
+      backgroundColor: "#fff",
+      zIndex: 1000,
+      flexWrap: "wrap",
+      top: 0,
+      left: 0,
+      boxSizing: "border-box",
+      padding: "0 65px",
+      height: "80px",
+    },
+    container: {
+      display: "flex",
+      width: "100%",
+      justifyContent: "space-between",
+      alignItems: "center",
+      flexWrap: "wrap",
+    },
+    logoSection: {
+      display: "flex",
+      alignItems: "center",
+      gap: "10px",
+    },
+    logoImage: {
+      width: "40px",
+      height: "25px",
+    },
+    logoText: {
+      fontSize: "1.5rem",
+      fontWeight: "bold",
+      color: "black",
+    },
+    nav: {
+      display: "flex",
+      gap: "60px",
+      listStyle: "none",
+      margin: 0,
+      padding: 0,
+    },
+    linkStyle: {
+      textDecoration: "none",
+      color: "#333",
+      fontWeight: 500,
+    },
+    buttonGroup: {
+      display: "flex",
+      gap: "10px",
+    },
+    loginBtn: {
+      padding: "0.6rem 1.2rem",
+      backgroundColor: "#fff",
+      color: "rgba(76, 175, 79, 1)",
+      borderRadius: "5px",
+      cursor: "pointer",
+      fontWeight: "bold",
+    },
+    signupBtn: {
+      padding: "0.6rem 1.2rem",
+      backgroundColor: "rgba(76, 175, 79, 1)",
+      color: "#fff",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      fontWeight: "bold",
+    },
   };
 
   return (
-    <header style={headerStyle}>
-      <div style={container}>
-        <div style={logoSection}>
-          <img src={icon} alt="Logo" style={logoImage} />
-          <div style={logoText}>Nexcent</div>
+    <header style={styles.headerStyle}>
+      <div style={styles.container}>
+        <div style={styles.logoSection}>
+          <img src={icon} alt="Logo" style={styles.logoImage} />
+          <div style={styles.logoText}>Nexcent</div>
         </div>
         <nav>
-          <ul style={nav}>
+          <ul style={styles.nav}>
             {[
               "Home",
               "Services",
@@ -98,20 +90,20 @@ const Header = () => {
               "FAQ",
             ].map((item) => (
               <li key={item}>
-                <a href="#" style={linkStyle}>
+                <a href="#" style={styles.linkStyle}>
                   {item}
                 </a>
               </li>
             ))}
           </ul>
         </nav>
-        <div style={buttonGroup}>
-          <button style={loginBtn}>Login</button>
-          <button style={signupBtn}>Sign Up</button>
+        <div style={styles.buttonGroup}>
+          <button style={styles.loginBtn}>Login</button>
+          <button style={styles.signupBtn}>Sign Up</button>
         </div>
       </div>
     </header>
   );
-};
+}
 
 export default Header;
