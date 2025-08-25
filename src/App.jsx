d27abb8017357c7a40c4c9ae4190b66a518de26d
Dhiyanesh88 <dhiyaneshrdhakrshnn@gmail.com>
2025-08-25 13:00:16 +0530

    webpage logo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import React from "react";
 import Header from "./webpages/header/header";
 import Hero from "./webpages/hero/hero";
 import Client from "./webpages/clients/client";
